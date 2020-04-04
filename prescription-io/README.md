ef2427ab40208bfb8e794804ba02e98120492d31
Vibhor Kanojia <vibhorkanojia@vibhorkanojia-macbookpro.roam.corp.google.com>
2020-04-04 14:15:45 +0200

    v1.0 of the prescription module. Refer the README for usage

--- prescription-io/README.md
+++ prescription-io/README.md
@@ -1,1 +1,16 @@
-This module starts a node js server which generates PDF based prescription templates on POST requests.
+Prescription pdf template generator module.
+It starts a node.js server that accepts POST requests and generates a pdf. 
+The pdf is then sent to the client as response.
+
+To install the npm modules:
+$ npm install
+
+To start the server:
+$ node index.js
+
+Sample request JSON format is included in the request.json file. Run this command to send a POST request:
+$ curl -H "Content-Type: application/json" --data @request.json http://localhost:3000/generateReport > prescription.pdf
+
+
+
+
--- prescription-io/index.js
+++ prescription-io/index.js
@@ -4,63 +4,15 @@ let ejs = require("ejs");
 let pdf = require("html-pdf");
 let path = require("path");
 
-let details = {
-    doctor: {
-        name: "Dr. Vidhan C Doshi",
-        details: "MBBS, MS - Opthalmology",
-        registration_number: "2011/02/0192",
-        location: "Mumbai",
-    },
-
-    patient: {
-        name: "Vibhor Kanojia",
-        age: "26",
-        gender: "Male",
-        location: "Pune",
-    },
-
-    prescription: {
-        issued_on: "02/04/2020",
-        id: "12345",
-        presenting_complaints: "Sore throat, mild fever, chest pains",
-        provisional_diagnosis: "Symptoms of X disease",
-        additional_remarks: "Gargle with salt water, drink hot water, wash hands. Avoid contact with other people, stay at home, and visit a doctor in case of breathing issues.",
-        follow_up_advice: "Like, share and subscribe.",
-        medicines: [
-                {
-                    name: "Medicine 1",
-                    frequency: "3",
-                    span: "2",
-                    usage: "After the meal",
-                },
-                {
-                    name: "Medicine 2",
-                    frequency: "2",
-                    span: "3",
-                    usage: "Before the meal",
-                },
-                {
-                    name: "Medicine 3",
-                    frequency: "1",
-                    span: "3",
-                    usage: "Before breakfast",
-                },
-                {
-                    name: "Medicine 4",
-                    frequency: "1",
-                    span: "2",
-                    usage: "Before sleeping",
-                },
-            ],
-    },
-};
-
 let footer = "<p style=\"color:#777777; text-align:center; font-size:7pt\">\
 Disclaimer: This prescription is based on the information provided by you in an online consultation and not on any physical verification. \
 Visit a doctor in case of emergency. This prescription is valid in India only. \
 <p>" 
 
-app.get("/generateReport", (req, res) => {
+app.use(express.json());
+app.listen(3000);
+app.post("/generateReport", (req, res) => {
+    var details = req.body;
     ejs.renderFile(path.join(__dirname, './views/', "prescription-template.ejs"), {details: details}, (err, data) => {
     if (err) {
 	  console.log(err);
@@ -77,15 +29,15 @@ app.get("/generateReport", (req, res) => {
                 "contents": footer
             },
         };
-        pdf.create(data, options).toFile("report.pdf", function (err, data) {
+        var file_name = "prescription-" + details.prescription.id + ".pdf";
+        pdf.create(data, options).toFile(file_name, function (err, data) {
             if (err) {
                 res.send(err);
             } else {
-                res.send("File created successfully");
+                res.download("./"+file_name);
             }
         });
     }
 });
 })
 
-app.listen(3000);
